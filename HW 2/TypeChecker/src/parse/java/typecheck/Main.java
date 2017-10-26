c6696d8179d9c5689d98e4feb74a78caf07280ed
jcherianucla <jcherianucla@github.com>
2017-10-26 10:35:04 -0700

    Initialize context visitor

--- HW 2/TypeChecker/src/parse/java/typecheck/Main.java
+++ HW 2/TypeChecker/src/parse/java/typecheck/Main.java
@@ -1,9 +1,29 @@
 package typecheck;
 
+import context.ContextVisitor;
+import context.MJTypeCheckException;
+import syntaxtree.Goal;
+
+import java.text.ParseException;
+
 public class Main {
     
     public static void main (String [] args) {
-        System.out.println("Program parsed successfully");
+        try {
+            // Parse
+            Goal goal = new MiniJavaParser(System.in).Goal();
+            // Build context table
+            ContextVisitor ctxVisitor = new ContextVisitor();
+            goal.accept(ctxVisitor, null);
+            ctxVisitor.context.printContextTable();
+            // Perform Type Check here
+        } catch (ParseException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        } catch (MJTypeCheckException e) {
+            System.out.print(e.getMessage());
+            System.exit(-1);
+        }
     }
 
 }
