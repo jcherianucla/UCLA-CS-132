10fc588fbf385e40e65f66014abd6265a2b55194
jcherianucla <jcherian@ucla.edu>
2017-10-30 19:32:34 -0700

    Add testcase for overriding methods

---
+++ HW 2/Typecheck/testcases/Jai-test.java
@@ -0,0 +1,23 @@
+class Main {
+		public static void main(String[] a) {
+		}
+}
+class C{
+    int x;
+    public int func (int a) {
+        boolean x;
+        int[] y;
+        x = true;
+        return a;
+    }
+}
+
+class B extends C {
+    boolean x;
+    public int func (int a) {
+        int x;
+        x = 1;
+        return 1;
+    }
+
+}
