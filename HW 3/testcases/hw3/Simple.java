dd522cd371f09ac13edecf9beec431097ab12b29
jcherianucla <jcherianucla@github.com>
2017-11-13 11:05:31 -0800

    Add if else test

--- HW 3/testcases/hw3/Simple.java
+++ HW 3/testcases/hw3/Simple.java
@@ -1,6 +1,22 @@
 class Main {
 	public static void main(String[] a){
-		System.out.println(10);
-		System.out.println(12);
+	    int x;
+	    x = 5;
+	    if (x < 7) {
+	        if (x < 3) {
+				System.out.println(0);
+				if (x < 4) {
+					x = 8;
+					System.out.println(1);
+				} else {
+					x = 110;
+					System.out.println(2);
+				}
+			} else {
+	        	System.out.println(3);
+			}
+		} else {
+	    	System.out.println(4);
+		}
 	}
 }
