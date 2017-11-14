7c1128b67a0831c299d2cba6d9ac0127c703cfb9
jcherianucla <jcherian@ucla.edu>
2017-11-14 11:19:52 -0800

    Add local array testcase

---
+++ HW 3/testcases/hw3/Simple-Array.java
@@ -0,0 +1,11 @@
+class Main {
+	public static void main(String[] a){
+	    int[] x;
+	    int j;
+	    j = 2;
+	    x = new int[10];
+	    x[j] = j * 2;
+	    System.out.println(x[j]);
+	    System.out.println(x.length);
+	}
+}
