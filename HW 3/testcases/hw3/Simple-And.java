6f94f0e72778cd4d28e2036ccfc27fcf6a195558
jcherianucla <jcherian@ucla.edu>
2017-11-14 22:31:29 -0800

    Add test for &&

---
+++ HW 3/testcases/hw3/Simple-And.java
@@ -0,0 +1,14 @@
+class Main {
+	public static void main(String[] a){
+	    int[] x;
+	    int j;
+	    j = 2;
+	    x = new int[10];
+	    x[j] = j * 2;
+	    if(((x[j]) < 5) && !((x.length) < 11)) {
+	    	System.out.println(100);
+	    } else {
+	    	System.out.println(0);
+	    }
+	}
+}
